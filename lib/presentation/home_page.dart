e52bfd6bad369fea4aa00a691ecb88a22d96490c
Oleksii Khoroshun <alexeyhoroshunvk@gmail.com>
2021-11-07 15:36:15 +0200

    refactor

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:teledart/teledart.dart';
-import 'package:teledart/telegram.dart';
 import 'package:telegram_bot/data/chat_id.dart';
 import 'package:telegram_bot/data/services/send_message_telegram.dart';
-import 'package:telegram_bot/data/token.dart';
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -39,7 +37,6 @@ class _MyHomePageState extends State<MyHomePage> {
               'Bot',
               style: Theme.of(context).textTheme.headline4,
             ),
-            const IconButtonTeledart(),
             Center(
               child: Column(
                 children: <Widget>[
@@ -85,36 +82,3 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class IconButtonTeledart extends StatelessWidget {
-  const IconButtonTeledart({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      onPressed: () async {
-        var telegram = Telegram(Token.agroMarketUkraineBot);
-        var event = Event((await telegram.getMe()).username!);
-        var teledart = TeleDart(telegram, event);
-        teledart.start();
-        teledart.onCommand('short').listen(((message) =>
-            teledart.replyMessage(message, 'Ответ на команду /short')));
-        teledart
-            .onMessage(entityType: 'bot_command', keyword: 'start')
-            .listen((message) => teledart.telegram.sendMessage(
-            message.chat.id, 'Ответ на команду /start'));
-        teledart
-            .onMessage(keyword: 'dart')
-            .where((message) =>
-        message.text?.contains('telegram') ?? false)
-            .listen((message) => teledart.replyPhoto(
-            message,
-            //  io.File('example/dash_paper_plane.png'),
-            'https://raw.githubusercontent.com/DinoLeung/TeleDart/master/example/dash_paper_plane.png',
-            caption: 'This is how Dash found the paper plane'));
-      },
-      icon: const Icon(Icons.play_arrow),
-    );
-  }
-}
